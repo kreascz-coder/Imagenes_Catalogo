c0448011a9463f666736e571632b5ffa183614cb
kreascz-coder <kreascz@gmail.com>
2026-05-21 16:09:12 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker - sin caché, siempre fresco
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', e => {});
